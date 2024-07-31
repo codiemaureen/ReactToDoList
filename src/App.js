8180a21bef76a2e6d9ade0791eb7208c4a40f9cd
Codiemaureen <codiestover.cs@gmail.com>
2024-07-31 15:22:41 -0400

    Adding add task feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -17,7 +18,12 @@ const App = () => {
     "reminder": true
   }
 ])
-
+  //add task 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => 
@@ -30,10 +36,11 @@ const App = () => {
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
-  
+
   return (
     <div className="container">
         <Header />
+        <AddTask onAdd={addTask}/>
         {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Task to Show')}
     </div>
   );
