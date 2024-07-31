e9dc71d6f49203f3dde68ffdd47a64c855cf5f60
Codiemaureen <codiestover.cs@gmail.com>
2024-07-31 15:37:37 -0400

    Fixing component format

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'Red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+      <Button 
+       color={showAdd ? 'Red' : 'Green'} 
+       text={showAdd ? 'Close' : 'Add'} 
+       onClick={onAdd}/>
     </header>
   )
 }
