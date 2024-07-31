9a0db3523d5659b14b005b2c877b332169472fc3
Codiemaureen <codiestover.cs@gmail.com>
2024-07-31 14:21:22 -0400

    Adding invidual Task Component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,22 +1,9 @@
-const tasks = [
-    {
-      "id": 1,
-      "text": "Doctors Appointment",
-      "day": "Feb 5th at 2:30pm",
-      "reminder": true
-    },
-    {
-      "id": 2,
-      "text": "Meeting at School",
-      "day": "Feb 6th at 1:30pm",
-      "reminder": true
-    }
-]
-const Tasks = () => {
+import Task from "./Task"
+const Tasks = ({ tasks }) => {
   return (
     <div>
       {tasks.map((task) => (
-       <h3 key={task.id}>{task.text}</h3>
+       <Task key={task.id} task={task}/>
        ))}
     </div>
   )
