93696a74282f56d039ab5ae43cb9f8b73e2aa4c8
Codiemaureen <codiestover.cs@gmail.com>
2024-07-31 12:42:09 -0400

    Adding header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-        <p>
-          Hello From React
-        </p>
+        <Header />
     </div>
   );
 }
