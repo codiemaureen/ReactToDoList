7a13322fdc8ec318df0f46ab27479030af2d6e86
Codiemaureen <codiestover.cs@gmail.com>
2024-07-31 15:56:54 -0400

    retrieve tasks from db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-  {
-    "id": 1,
-    "text": "Doctors Appointment",
-    "day": "Feb 5th at 2:30pm",
-    "reminder": true
-  },
-  {
-    "id": 2,
-    "text": "Meeting at School",
-    "day": "Feb 6th at 1:30pm",
-    "reminder": true
-  }
-])
+  const [tasks, setTasks] = useState([])
+
+  //load task from database on pageload
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTask()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+
+  //fetch task
+    const fetchTask = async() => {
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json();
+      return data;
+    }
   //add task 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
