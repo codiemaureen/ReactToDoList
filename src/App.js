732694137878117887dbccbf25a1b257acdbc601
Codiemaureen <codiestover.cs@gmail.com>
2024-08-01 15:01:42 -0400

    Reapply "Adding routes"
    This reverts commit 41dd7d520ba26182a547adaf27787319d6c436da.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -70,12 +72,21 @@ const App = () => {
 
 
   return (
-    <div className="container">
+    <Router>
+      <div className="container">
         <Header onAdd={() => {setShowAddTask(!showAddTask)}} showAdd={showAddTask}/>
-        {showAddTask && <AddTask onAdd={addTask}/>}
-        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Task to Show')}
+        <Routes>
+          <Route path="/" element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask}/>}
+              {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Task to Show')}
+            </>
+          }/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
         <Footer />
-    </div>
+      </div>
+    </Router>
   );
 }
 
