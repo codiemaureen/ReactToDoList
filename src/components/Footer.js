94a5ff1eee2493593b3184fe45edcdac14ece58c
Codiemaureen <codiestover.cs@gmail.com>
2024-08-01 15:27:22 -0400

    Temp hide the about link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <p>Copyright &copy; 2024</p>
-      <a href="/about">About</a>
+      {/* <a href="/about">About</a> */}
     </footer>
   )
 }
